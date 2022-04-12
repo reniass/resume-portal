5aa562a3efd716dbf3b80b7eb9a6c29b72ac321b
reniass <reinke.krzysztof@o2.pl>
2022-04-12 14:14:06 +0200

    Adding user and repository JPA infrastracture

---
+++ src/main/java/io/javabrains/resumeportal/UserRepository.java
@@ -0,0 +1,12 @@
+package io.javabrains.resumeportal;
+
+import io.javabrains.resumeportal.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository  extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
