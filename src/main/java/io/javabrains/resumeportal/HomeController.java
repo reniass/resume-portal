7c426ed3725868ee0f54d514bc8bb8e170133222
reniass <reinke.krzysztof@o2.pl>
2022-04-17 18:51:01 +0200

    Added user profile entity, user profile repository and fetching to show theme and summary

--- src/main/java/io/javabrains/resumeportal/HomeController.java
+++ src/main/java/io/javabrains/resumeportal/HomeController.java
@@ -1,5 +1,7 @@
 package io.javabrains.resumeportal;
 
+import io.javabrains.resumeportal.models.UserProfile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserProfileRepository userProfileRepository;
 
     @GetMapping("/")
     @ResponseBody
@@ -24,7 +31,12 @@ public class HomeController {
 
     @GetMapping("/view/{userId}")
     public String view(@PathVariable String userId, Model model) {
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userId);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("not found: " + userId));
+
+        UserProfile userProfile = userProfileOptional.get();
         model.addAttribute("userId", userId);
+        model.addAttribute("userProfile", userProfile);
         return "profile-templates/2/index";
     }
 }
--- src/main/java/io/javabrains/resumeportal/ResumePortalApplication.java
+++ src/main/java/io/javabrains/resumeportal/ResumePortalApplication.java
@@ -1,6 +1,7 @@
 package io.javabrains.resumeportal;
 
 import io.javabrains.resumeportal.models.User;
+import io.javabrains.resumeportal.models.UserProfile;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@EnableJpaRepositories
 public class ResumePortalApplication {
 
 	public static void main(String[] args) {
@@ -17,10 +18,13 @@ public class ResumePortalApplication {
 
 
 	@Bean
-	CommandLineRunner runner(UserRepository userRepository) {
+	CommandLineRunner runner(UserRepository userRepository, UserProfileRepository userProfileRepository) {
 		return args -> {
 			userRepository.save(new User("foo", "foo", true, "USER"));
 			userRepository.save(new User("bar", "bar", true, "USER"));
+
+			userProfileRepository.save(new UserProfile("foo", 1, "User name foo"));
+			userProfileRepository.save(new UserProfile("bar", 2, "User name bar"));
 		};
 	}
 
